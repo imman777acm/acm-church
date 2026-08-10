e02efbfddb36eb28532e32b572b8d9e7eceea90b
imman777acm <imman777@gmail.com>
2026-08-10 23:27:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="acm-church-v2";
+const CACHE="acm-church-v3";
 const ASSETS=["./","./index.html","./style.css","./manifest.webmanifest","./logo.png","./church.jpg","./pastor.jpg","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
